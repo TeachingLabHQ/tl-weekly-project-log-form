77356b7342442f8dbc836a9fd069b1486249446d
YCPyc <yp9ar@virginia.edu>
2025-02-06 20:24:27 -0500

    added a budgeted hour expectation remidner and sorted the project names

--- app/domains/project/service.ts
+++ app/domains/project/service.ts
@@ -3,7 +3,16 @@ import { ProjectRepository } from "./repository";
 
 export function projectService(projectRepository: ProjectRepository) {
   return {
-    fetchAllProjects: projectRepository.fetchAllProjects,
+    fetchAllProjects: async () => {
+      const { data: allProjects } = await projectRepository.fetchAllProjects();
+      allProjects
+        ?.find((project) => project.projectType === "Program-related Project")
+        ?.projects.sort((a, b) => a.localeCompare(b));
+      allProjects
+        ?.find((project) => project.projectType === "Internal Project")
+        ?.projects.sort((a, b) => a.localeCompare(b));
+      return { data: allProjects };
+    },
     fetchProgramProjectsStaffing: projectRepository.fetchProgramProjects,
     fetchProgramProjectsWithHours:
       projectRepository.fetchProgramProjectWithHours,
