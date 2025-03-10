4cf2eac77c0f2b203e2bbc8a7d58c7c542403e1f
YCPyc <yp9ar@virginia.edu>
2025-03-09 22:55:47 -0400

    update date logic

--- app/utils/utils.tsx
+++ app/utils/utils.tsx
@@ -7,6 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDate = (date: Date) => {
   const dateValue = new Date(date);
+  dateValue.setHours(12, 0, 0, 0);
   const month = ("0" + (dateValue.getMonth() + 1)).slice(-2);
   const day = ("0" + dateValue.getDate()).slice(-2);
   const year = dateValue.getFullYear();
